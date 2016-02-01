b6b6d007609356b16d4bda38e2de4f3416e765dc
David Steele <dsteele@gmail.com>
2016-02-01 07:41:07 -0500

    mdns.py fixes
    - Bug fix - don't leave cruft at end of 'hosts'.
    - Support for publishing service file.
    - Edit avahi-daemon.conf to disable self host address publishing,
      to avoid having multiple host names for my address. The collision
      in the reverse entry caused a failure.
    - Add a comitup publish helper function.

--- comitup/mdns.py
+++ comitup/mdns.py
@@ -1,23 +1,94 @@
 #!/usr/bin/python
 
 import argparse
+import textwrap
+import jinja2
+import ConfigParser
+
+
+def set_avahi_param(section, parameter, value, conf='/etc/avahi/avahi-daemon.conf'):
+
+    out = []
+    with open(conf, 'r') as fp:
+        for line in fp.readlines():
+            if parameter not in line:
+                out.append(line)
+
+            if '[' + section + ']' in line:
+                out.append(parameter + "=" + value + "\n")
+
+    with open(conf, 'w') as fp:
+        for line in out:
+            fp.write(line)
+
+
+def init_mdns(conf='/etc/avahi/avahi-daemon.conf'):
+    set_avahi_param('publish', 'publish-addresses', 'no')
 
 
 def update_entry(name, addr=None, file='/etc/avahi/hosts'):
 
     with open(file, 'r+') as fp:
-        lines = [x for x in fp.readlines() if not name + '\n' in x]
 
-        if addr:
-            lines.append("{0} {1}\n".format(addr, name))
+        if isinstance(name, basestring):
+            name = [name]
+
+        lines = [x for x in fp.readlines()]
+        for host in name:
+            lines = [x for x in lines if not host in x]
+            if addr:
+                lines.append("{0} {1}\n".format(addr, host))
 
         fp.seek(0)
         for line in lines:
             fp.write(line)
+        fp.truncate()
 
 
 def rm_entry(name, file='/etc/avahi/hosts'):
     update_entry(name, None, file)
+
+
+xml_template = textwrap.dedent(
+"""    <?xml version="1.0" standalone='no'?>
+    <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+    <service-group>
+        <name replace-wildcards="yes">{{ service_name }}</name>
+    {%- for service in services -%}
+    {% for host in hosts %}
+        <service>
+            <host-name>{{ host }}</host-name>
+            <type>{{ service.type }}</type>
+            <port>{{ service.port }}</port>
+        </service>
+    {%- endfor -%}
+    {% endfor %}
+    </service-group>
+    """
+)
+
+def mk_service_file(data, path="/etc/avahi/services/comitup.service"):
+    template = jinja2.Template(xml_template)
+
+    with open(path, 'w') as fp:
+        fp.write(template.render(data))
+
+
+def publish_comitup_hosts(hosts, addr):
+    data = {
+        'service_name': "comitup on %h",
+        'hosts': hosts,
+        'services': [
+            {
+                'type': '_workstation._tcp',
+                'port': 9,
+            },
+        ],
+    }
+
+    update_entry(hosts, addr)
+
+    mk_service_file(data)
 
 
 def parse_args():
@@ -51,7 +122,9 @@ def main():
     if '.local' not in host:
         host += ".local"
 
-    update_entry(args.host, args.address)
+    init_mdns()
+
+    publish_comitup_hosts([host], args.address)
 
 
 if __name__ == '__main__':
--- test/test_mdns.py
+++ test/test_mdns.py
@@ -3,6 +3,10 @@ import pytest
 from comitup import mdns
 import textwrap
 
+from xml.etree import ElementTree as ET
+
+import os
+
 
 @pytest.fixture()
 def avahi_conf(tmpdir):
@@ -20,6 +24,27 @@ def avahi_conf(tmpdir):
         )
 
     return path
+
+
+@pytest.fixture()
+def service_path(tmpdir):
+    return tmpdir.join(u'comitup.service')
+
+
+@pytest.fixture()
+def service_data(service_path):
+    data = {
+        'service_name': "comitup on %h",
+        'hosts': ['host1', 'host2'],
+        'services': [
+            {
+                'type': '_http._tcp',
+                'port': 80,
+            },
+        ],
+    }
+
+    return data
 
 
 def test_avahi_fxt(avahi_conf):
@@ -44,6 +69,30 @@ def test_update_entry(avahi_conf):
     assert "9.8.7.6 comitup.local\n" not in new
 
 
+@pytest.mark.parametrize("hosts", (
+        ('comitup.local', 'comitup-8675.local'),
+        ('comitup-8675.local', 'comitup.local'),
+    )
+)
+def test_update_entries(avahi_conf, hosts):
+    mdns.update_entry(
+        hosts,
+        '10.11.12.13',
+        avahi_conf.strpath
+    )
+
+    with open(avahi_conf.strpath, 'r') as fp:
+        new = fp.read()
+
+    assert "10.11.12.13 comitup.local\n" in new
+    assert "10.11.12.13 comitup-8675.local\n" in new
+
+    assert "header" in new
+    assert "1.2.3.4" in new
+    assert "5.6.7.8" in new
+    assert "9.8.7.6 comitup.local\n" not in new
+
+
 def test_rm_entry(avahi_conf):
     mdns.rm_entry('comitup.local', avahi_conf.strpath)
 
@@ -54,3 +103,26 @@ def test_rm_entry(avahi_conf):
     assert "1.2.3.4" in new
     assert "5.6.7.8" in new
     assert "comitup.local" not in new
+
+
+def test_avahi_service_exist(service_path, service_data):
+    mdns.mk_service_file(service_data, service_path.strpath)
+    assert os.path.exists(service_path.strpath)
+
+
+def test_avahi_service_parses(service_path, service_data):
+    mdns.mk_service_file(service_data, service_path.strpath)
+    ET.parse(service_path.strpath)
+
+
+def test_avahi_service_content(service_path, service_data):
+    mdns.mk_service_file(service_data, service_path.strpath)
+    tree = ET.parse(service_path.strpath)
+    root = tree.getroot()
+
+    assert(root.tag == 'service-group')
+    assert(root[0].tag == 'name')
+    assert('comitup' in root[0].text)
+    assert(root[1].tag == 'service')
+    assert(root[1][0].text == 'host1')
+    assert(root[2][0].text == 'host2')
