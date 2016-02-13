a3364b0584f52357f1e375d8c1bfdcf2ed616358
David Steele <dsteele@gmail.com>
2016-02-13 16:10:19 -0500

    Fix dict override in test_persist.py

--- test/test_persist.py
+++ test/test_persist.py
@@ -12,14 +12,14 @@ def jsonpath(tmpdir):
 
 
 def test_persist_is_dict(jsonpath):
-    dict = persist(jsonpath)
-    dict['a'] = 'b'
-    assert dict['a'] == 'b'
+    mydict = persist(jsonpath)
+    mydict['a'] = 'b'
+    assert mydict['a'] == 'b'
 
 
 def test_persist_default(jsonpath):
-    dict = persist(jsonpath, {'a': 'b'})
-    assert dict['a'] == 'b'
+    mydict = persist(jsonpath, {'a': 'b'})
+    assert mydict['a'] == 'b'
 
 
 def test_persist_default_persists(jsonpath):
@@ -35,21 +35,21 @@ def test_persist_override_default(jsonpath):
 
 
 def test_persist_override_default2(jsonpath):
-    dict = persist(jsonpath, {'a': 'a'})
-    dict['a'] = 'b'
+    mydict = persist(jsonpath, {'a': 'a'})
+    mydict['a'] = 'b'
     new = persist(jsonpath, {'a': 'c'})
     assert new['a'] == 'b'
 
 
 def test_persist_update(jsonpath):
-    dict = persist(jsonpath, {'a': 'a'})
-    dict.update({'a': 'b'})
+    mydict = persist(jsonpath, {'a': 'a'})
+    mydict.update({'a': 'b'})
     new = persist(jsonpath, {'a': 'c'})
     assert new['a'] == 'b'
 
 
 def test_persist_setdefault(jsonpath):
-    dict = persist(jsonpath)
-    dict.setdefault('a', 'b')
+    mydict = persist(jsonpath)
+    mydict.setdefault('a', 'b')
     new = persist(jsonpath, {'a': 'c'})
     assert new['a'] == 'b'
