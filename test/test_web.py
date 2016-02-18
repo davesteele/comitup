c2754a2899fc75f7aeb62f5c7eb20004a31e7373
David Steele <dsteele@gmail.com>
2016-02-18 14:36:09 -0500

    test_web: Add checksum tests

--- test/test_web.py
+++ test/test_web.py
@@ -6,6 +6,30 @@ import string
 from web import stringify as sfy
 
 
+@pytest.mark.parametrize(
+                "test_str",
+                ("", "hello", ''.join('a' for i in range(999)))
+             )
+def test_web_stringify_encrypt(test_str):
+    assert test_str == sfy.decrypt(sfy.encrypt(test_str))
+
+    
+@pytest.mark.parametrize('tst', ('len', 'pass', 'fail'))
+def test_web_stringify_chk():
+    orig = 'a'
+    chked = sfy.add_chk(orig)
+    
+    if tst == 'len':
+        assert len(chked) > len(orig)
+    elif tst == 'pass':
+        assert sfy.chk_chk(chked) == orig
+    elif tst == 'fail':
+        with raises sfy.SfyException:
+            sfy.chk_chk(chked + 'a')
+    else:
+        assert False
+        
+        
 @pytest.mark.parametrize("var", (1, "string", {'a': 1, 'b': 2}))
 @pytest.mark.parametrize("test", ("encode", "decode", "iv"))
 def test_web_stringify(test, var):
@@ -17,11 +41,3 @@ def test_web_stringify(test, var):
         assert sfy.decode(encoded) == var
     elif test == "iv":
         assert encoded != sfy.encode(var)
-
-
-@pytest.mark.parametrize(
-                "test_str",
-                ("", "hello", ''.join('a' for i in range(999)))
-             )
-def test_web_stringify_encrypt(test_str):
-    assert test_str == sfy.decrypt(sfy.encrypt(test_str))
