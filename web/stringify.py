8d92d4a41aef731d3d73f9ca407553d09288278c
David Steele <dsteele@gmail.com>
2016-02-21 12:51:00 -0500

    stringify: PEP8

--- web/stringify.py
+++ web/stringify.py
@@ -12,10 +12,11 @@ key = crand.read(AES.block_size)
 
 
 class SfyException(Exception):
-     def __init__(self, value):
-         self.value = value
-     def __str__(self):
-         return repr(self.value)
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 
 def encrypt(str):
