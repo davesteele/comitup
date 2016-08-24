1d7206a25648f2c93a6d470604bbdda477902c99
David Steele <steele@debian.org>
2016-08-24 13:30:51 -0400

    Update version to 0.5

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ class MyClean(clean):
 setup(
     name='comitup',
     packages=['comitup', 'web', 'cli'],
-    version='0.4',
+    version='0.5',
     description="Copy a remote file using multiple SSH streams",
     classifiers=[
         'Development Status :: 1 - Planning',
