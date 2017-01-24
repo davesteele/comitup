a5c5c7e4d8d5fbaef8d9962dbdda4beebe382cd1
David Steele <steele@debian.org>
2017-01-23 21:21:49 -0500

    Set version to 0.7

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class MyClean(clean):
 setup(
     name='comitup',
     packages=['comitup', 'web', 'cli'],
-    version='0.5',
+    version='0.7',
     description="Copy a remote file using multiple SSH streams",
     classifiers=[
         'Development Status :: 1 - Planning',
