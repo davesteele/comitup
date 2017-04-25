b28a1d6d185a3e207575c2d9778dc60cb2c8431b
David Steele <dsteele@gmail.com>
2017-04-25 02:29:05 +0000

    Start in HOTSPOT mode
    To make sure there is a hotspot present in the multiple wifi adapter
    mode.

--- comitup/statemgr.py
+++ comitup/statemgr.py
@@ -93,7 +93,7 @@ def init_state_mgr(gconf, gdata, callbacks):
     states.init_states(get_hosts(conf, data), callbacks)
     com_obj = Comitup()
 
-    states.set_state('CONNECTING', states.candidate_connections())
+    states.set_state('HOTSPOT')
 
 
 def main():
@@ -104,7 +104,7 @@ def main():
     log.info('starting')
 
     init_state_mgr('comitup.local', 'comitup-1111.local')
-    states.set_state('CONNECTING', states.candidate_connections())
+    states.set_state('HOTSPOT')
 
     loop = gobject.MainLoop()
     loop.run()
