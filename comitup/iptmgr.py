8b48705ab65d664b0c778c370f40141934b05458
David Steele <steele@debian.org>
2017-07-04 09:43:27 -0400

    Add appliance iptables router rules
    When 'CONNECTED' with multiple wifi devices, enable forwarding
    and masquerade.

--- comitup/iptmgr.py
+++ comitup/iptmgr.py
@@ -8,9 +8,11 @@
 
 import logging
 import subprocess
+import modemgr
 
 
 start_cmds = [
+    # HOTSPOT rules
     "iptables -N COMITUP-OUT",
     "iptables -A COMITUP-OUT "
       "-p icmp --icmp-type destination-unreachable -j DROP", # noqa
@@ -19,6 +21,7 @@ start_cmds = [
 ]
 
 end_cmds = [
+    # Clear HOTSPOT rules
     "iptables -D OUTPUT -j COMITUP-OUT",
     "iptables -D COMITUP-OUT "
         "-p icmp --icmp-type destination-unreachable -j DROP", #noqa
@@ -26,18 +29,43 @@ end_cmds = [
     "iptables -X COMITUP-OUT",
 ]
 
+appliance_cmds = [
+    "iptables -t nat -N COMITUP-FWD",
+    "iptables -t nat -A COMITUP-FWD -o wlan1 -j MASQUERADE",
+    "iptables -t nat -A COMITUP-FWD -j RETURN",
+    "iptables -t nat -A POSTROUTING -j COMITUP-FWD",
+    "echo 1 > /proc/sys/net/ipv4/ip_forward",
+]
+
+appliance_clear = [
+    "iptables -t nat -D POSTROUTING -j COMITUP-FWD",
+    "iptables -t nat -D COMITUP-FWD -o wlan1 -j MASQUERADE",
+    "iptables -t nat -D COMITUP-FWD -j RETURN",
+    "iptables -t nat -X COMITUP-FWD",
+]
+
+
 log = logging.getLogger('comitup')
 
 
 def run_cmds(cmds):
-    [subprocess.call(cmd.split()) for cmd in cmds]
+    [subprocess.call(cmd, shell=True) for cmd in cmds]
 
 
 def state_callback(state, action):
     if (state, action) == ('HOTSPOT', 'start'):
+        run_cmds(end_cmds)
         run_cmds(start_cmds)
+
+        if modemgr.get_mode() == 'router':
+            run_cmds(appliance_clear)
+
     elif (state, action) == ('CONNECTING', 'start'):
         run_cmds(end_cmds)
+
+        if modemgr.get_mode() == 'router':
+            run_cmds(appliance_clear)
+            run_cmds(appliance_cmds)
 
 
 def init_iptmgr():
