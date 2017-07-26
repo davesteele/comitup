212be8175235d78f00ff47312c6cc3b82b61e671
David Steele <dsteele@gmail.com>
2017-07-26 02:01:06 +0000

    Add the -w option to iptables commands
    They were colliding with whatever else is managing iptables, and
    rules were failing randomly.

--- comitup/iptmgr.py
+++ comitup/iptmgr.py
@@ -13,36 +13,41 @@ import modemgr
 
 start_cmds = [
     # HOTSPOT rules
-    "iptables -N COMITUP-OUT",
-    "iptables -A COMITUP-OUT "
+    "iptables -w -N COMITUP-OUT",
+    "iptables -w -A COMITUP-OUT "
       "-p icmp --icmp-type destination-unreachable -j DROP",  # noqa
-    "iptables -A COMITUP-OUT -j RETURN",
-    "iptables -I OUTPUT -j COMITUP-OUT",
+    "iptables -w -A COMITUP-OUT "
+      "-p icmp --icmp-type port-unreachable -j DROP",  # noqa
+    "iptables -w -A COMITUP-OUT -j RETURN",
+    "iptables -w -I OUTPUT -j COMITUP-OUT",
 ]
 
 end_cmds = [
     # Clear HOTSPOT rules
-    "iptables -D OUTPUT -j COMITUP-OUT >/dev/null 2>&1",
-    "iptables -D COMITUP-OUT "
+    "iptables -w -D OUTPUT -j COMITUP-OUT >/dev/null 2>&1",
+    "iptables -w -D COMITUP-OUT "
         "-p icmp --icmp-type destination-unreachable "        # noqa
         "-j DROP >/dev/null 2>&1",                            # noqa
-    "iptables -D COMITUP-OUT -j RETURN >/dev/null 2>&1",
-    "iptables -X COMITUP-OUT >/dev/null 2>&1",
+    "iptables -w -D COMITUP-OUT "
+        "-p icmp --icmp-type port-unreachable "        # noqa
+        "-j DROP >/dev/null 2>&1",                            # noqa
+    "iptables -w -D COMITUP-OUT -j RETURN >/dev/null 2>&1",
+    "iptables -w -X COMITUP-OUT >/dev/null 2>&1",
 ]
 
 appliance_cmds = [
-    "iptables -t nat -N COMITUP-FWD",
-    "iptables -t nat -A COMITUP-FWD -o wlan1 -j MASQUERADE",
-    "iptables -t nat -A COMITUP-FWD -j RETURN",
-    "iptables -t nat -A POSTROUTING -j COMITUP-FWD",
+    "iptables -w -t nat -N COMITUP-FWD",
+    "iptables -w -t nat -A COMITUP-FWD -o wlan1 -j MASQUERADE",
+    "iptables -w -t nat -A COMITUP-FWD -j RETURN",
+    "iptables -w -t nat -A POSTROUTING -j COMITUP-FWD",
     "echo 1 > /proc/sys/net/ipv4/ip_forward",
 ]
 
 appliance_clear = [
-    "iptables -t nat -D POSTROUTING -j COMITUP-FWD >/dev/null 2>&1",
-    "iptables -t nat -D COMITUP-FWD -o wlan1 -j MASQUERADE >/dev/null 2>&1",
-    "iptables -t nat -D COMITUP-FWD -j RETURN >/dev/null 2>&1",
-    "iptables -t nat -X COMITUP-FWD >/dev/null 2>&1",
+    "iptables -w -t nat -D POSTROUTING -j COMITUP-FWD >/dev/null 2>&1",
+    "iptables -w -t nat -D COMITUP-FWD -o wlan1 -j MASQUERADE >/dev/null 2>&1",
+    "iptables -w -t nat -D COMITUP-FWD -j RETURN >/dev/null 2>&1",
+    "iptables -w -t nat -X COMITUP-FWD >/dev/null 2>&1",
 ]
 
 
