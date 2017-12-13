0c093e4225a6b696ae57e2a76e896b7d162cda3d
David Steele <steele@debian.org>
2017-12-12 21:32:25 -0500

    Get the tests working with Python3

--- test/test_mdns.py
+++ test/test_mdns.py
@@ -67,10 +67,10 @@ def test_avahi_add_hosts(avahi_fxt):
 
 
 @pytest.mark.parametrize("dns_in, dns_out", (
-    ("a.b.c", "a.b.c"),
-    ("A.B.C", "A.B.C"),
-    ("a..b", "a.b"),
-    ("a.b.", "a.b"),
+    ("a.b.c", "a.b.c".encode()),
+    ("A.B.C", "A.B.C".encode()),
+    ("a..b", "a.b".encode()),
+    ("a.b.", "a.b".encode()),
 ))
 def test_avahi_encode_dns(dns_in, dns_out):
     assert dns_out == mdns.encode_dns(dns_in)
