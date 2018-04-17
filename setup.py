83eef6c0403ecbe835d68bb17cf6a1c297d5ddcd
David Steele <dsteele@gmail.com>
2018-04-17 08:03:57 -0400

    Set version to 1.3

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ class MyClean(clean):
 setup(
     name='comitup',
     packages=['comitup', 'web', 'cli'],
-    version='1.2.5',
+    version='1.3',
     description="Remotely manage wifi connections on a headless computer",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
