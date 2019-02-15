f14fa0b411d2ecff43731404dff1a40149cb6981
David Steele <dsteele@gmail.com>
2019-02-15 17:45:13 -0500

    Replace dead tracker

--- releaseimg.py
+++ releaseimg.py
@@ -23,9 +23,10 @@ if ".zip" not in zip_name:
 
 # Update tracker list from https://newtrackon.com
 trackers = [
-    'udp://tracker.vanitycore.co:6969/announce',
     'udp://tracker.coppersurfer.tk:6969/announce',
     'http://torrent.nwps.ws:80/announce',
+    'udp://thetracker.org:80/announce',
+#    'udp://tracker.vanitycore.co:6969/announce',
 #    'http://retracker.telecom.by:80/announce',
 #    'http://share.camoe.cn:8080/announce',
 #    'https://1.track.ga:443/announce',
