92b12aeed1a9534f7b004390e0095080a6a7fd62
David Steele <dsteele@gmail.com>
2019-06-06 15:11:08 -0400

    Nox test: Verify proper OS packages are present

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,15 @@
 
 import nox
+import subprocess
+import sys
+
+pkgs = [
+    "libcairo2-dev",
+    "libjpeg-dev",
+    "libgif-dev",
+    "gobject-introspection",
+    "libgirepository1.0-dev",
+]
 
 deps = [
     "pytest",
@@ -7,11 +17,25 @@ deps = [
     "dbus-python",
     "python-networkmanager",
     "flask",
+    # apt-get install libcairo2-dev libjpeg-dev libgif-dev
+    # apt-get install gobject-introspection libgirepository1.0-dev
     "pygobject",
 ]
 
+def missing_pkg(pkg):
+    cmd = "dpkg -l {} > /dev/null".format(pkg)
+    return subprocess.run(cmd, shell=True).returncode != 0
+
 @nox.session(python="3.7")
 def test(session):
+    missings = [x for x in pkgs if missing_pkg(x)]
+    if missings:
+        print("Missing packages")
+        for pkg in missings:
+            print("    {}".format(pkg))
+        sys.exit(1)
+
     for pkg in deps:
         session.install(pkg)
+
     session.run("pytest")
