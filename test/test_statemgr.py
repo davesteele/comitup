1f1de8bb00cba1fa21ff845cf6b784b79de378a0
sistemicorp <martin.guthrie@sistemi.ca>
2021-04-03 11:09:28 -0400

    add mac and serial number cases

--- test/test_statemgr.py
+++ test/test_statemgr.py
@@ -80,6 +80,16 @@ Case = namedtuple("ApName", ["input", "out"])
         Case("<hostname>", "host"),
         Case("<hostname>-<n>", "host-1"),
         Case("<bogus>", "<bogus>"),
+        Case("<M>", "1"),
+        Case("<MMMMMM>", "654321"),
+        Case("<hostname>-<MMMMMM>", "host-654321"),
+        Case("<MMMM>-<hostname>", "4321-host"),
+        Case("<MMMM><hostname>", "4321host"),
+        Case("<s>", "1"),
+        Case("<ssssss>", "654321"),
+        Case("<hostname>-<ssssss>", "host-654321"),
+        Case("<ssss>-<hostname>", "4321-host"),
+        Case("<ssss><hostname>", "4321host"),
     ]
 )
 def test_expand_ap(ap_name_fxt, case):
@@ -88,4 +98,6 @@ def test_expand_ap(ap_name_fxt, case):
 
     data=Object()
     data.id = "1234"
+    data.mac = "CBA987654321"
+    data.sn = "1000000087654321"
     assert sm.expand_ap(case.input, data) == case.out
