2255dabc4defea0fc2ec1f998356a5ee6e771ce0
David Steele <steele@debian.org>
2021-05-18 14:11:53 -0400

    Set version to v1.25

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class MyClean(clean):
 setup(
     name='comitup',
     packages=['comitup', 'web', 'cli'],
-    version='1.24',
+    version='1.25',
     description="Remotely manage wifi connections on a headless computer",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
