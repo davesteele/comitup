0d5c02315a1492941c75444bf90231aa63e690cc
David Steele <steele@debian.org>
2021-06-13 12:34:43 -0400

    Fix the entry point for the cli

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     entry_points={
         "console_scripts": [
             "comitup=comitup.comitup:main",
-            "comitup-cli=cli.comitupcli:interpreter",
+            "comitup-cli=cli.comitupcli:main",
             "comitup-web=web.comitupweb:main",
         ],
     },
