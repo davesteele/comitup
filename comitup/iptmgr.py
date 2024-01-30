bb92584552ccb6bf5f268729ec1003fd66acad74
David Steele <dsteele@gmail.com>
2024-01-30 12:35:39 -0500

    Replace iptables with nftables

--- comitup/iptmgr.py
+++ comitup/iptmgr.py
@@ -15,94 +15,103 @@ from typing import List, Optional
 
 from comitup import modemgr, nm, routemgr
 
-# fmt: off
-start_cmds: List[str] = [
-    # HOTSPOT rules
-    "iptables -w -N COMITUP-OUT",
-    "iptables -w -A COMITUP-OUT "
-      "-p icmp --icmp-type destination-unreachable -j DROP",  # noqa
-    "iptables -w -A COMITUP-OUT "
-      "-p icmp --icmp-type port-unreachable -j DROP",
-    "iptables -w -A COMITUP-OUT -j RETURN",
-    "iptables -w -I OUTPUT -o {ap} -j COMITUP-OUT",
-]
-
-end_cmds: List[str] = [
-    # Clear HOTSPOT rules
-    "iptables -w -D OUTPUT -o {ap} -j COMITUP-OUT >/dev/null 2>&1",
-    "iptables -w -F COMITUP-OUT >/dev/null 2>&1",
-    "iptables -w -X COMITUP-OUT >/dev/null 2>&1",
-]
-
-appliance_cmds: List[str] = [
-    "iptables -w -t nat -N COMITUP-FWD",
-    "iptables -w -t nat -A COMITUP-FWD -o {link} -j MASQUERADE",
-    "iptables -w -t nat -A COMITUP-FWD -j RETURN",
-    "iptables -w -t nat -A POSTROUTING -j COMITUP-FWD",
-    "echo 1 > /proc/sys/net/ipv4/ip_forward",
-]
-
-appliance_clear: List[str] = [
-    "iptables -w -t nat -D POSTROUTING -j COMITUP-FWD >/dev/null 2>&1",
-    "iptables -w -t nat -F COMITUP-FWD >/dev/null 2>&1",
-    "iptables -w -t nat -X COMITUP-FWD >/dev/null 2>&1",
-]
-# fmt: on
-
 log: logging.Logger = logging.getLogger("comitup")
 
 
-def run_cmds(cmds: List[str]) -> None:
+def start_hs_rules() -> None:
+    # fmt: off
+    start_cmds: List[str] = [
+        "nft add table ip filter",
+        "nft 'add chain ip filter COMITUP-OUT {{ type filter hook output priority 100 ; }}'",
+        "nft 'add rule ip filter COMITUP-OUT icmp type destination-unreachable counter drop' ",
+        "nft 'add rule ip filter COMITUP-OUT icmp code port-unreachable counter drop' ",
+        "nft 'add rule ip filter COMITUP-OUT counter return'",
+    ]
+    # fmt: on
+
+    ap_dev = nm.device_name(modemgr.get_ap_device())
+    run_cmds(start_cmds, ap=ap_dev)
+
+
+def stop_hs_rules() -> None:
+    # fmt: off
+    end_cmds: List[str] = [
+        "nft flush chain ip filter COMITUP-OUT  >/dev/null 2>&1",
+        "nft delete chain ip filter COMITUP-OUT > /dev/null 2>&1",
+    ]
+    # fmt: on
+
+    run_cmds(end_cmds)
+
+
+def start_router_rules() -> None:
+    # fmt: off
+    appliance_cmds: List[str] = [
+        "nft add table ip nat",
+        "nft 'add chain nat COMITUP-FWD {{ type nat hook postrouting priority 100 ; }}'",
+        "nft 'add rule ip nat COMITUP-FWD oifname {link} counter masquerade'",
+        "nft 'add rule ip nat COMITUP-FWD counter return'",
+        "echo 1 > /proc/sys/net/ipv4/ip_forward",
+    ]
+    # fmt: on
+
+    link_dev = nm.device_name(modemgr.get_link_device())
+    run_cmds(appliance_cmds, link=link_dev)
+
+
+def stop_router_rules() -> None:
+    # fmt: off
+    appliance_clear: List[str] = [
+        "nft flush chain ip nat COMITUP-FWD > /dev/null 2>&1",
+        "nft delete chain ip nat COMITUP-FWD > /dev/null 2>&1",
+    ]
+    # fmt: on
+
+    run_cmds(appliance_clear)
+
+
+def run_cmds(cmds: List[str], **vars) -> None:
     linkdev = nm.device_name(modemgr.get_link_device())
+    vars["link"] = linkdev
     apdev = nm.device_name(modemgr.get_ap_device())
+    vars["ap"] = apdev
     for cmd in cmds:
-        subprocess.call(cmd.format(link=linkdev, ap=apdev), shell=True)
+        runcmd = cmd.format(**vars)
+        log.debug(f'iptmgr - running "{runcmd}"')
+        subprocess.call(runcmd, shell=True)
 
 
 def state_callback(state: str, action: str) -> None:
     if (state, action) == ("HOTSPOT", "start"):
-        log.debug("Running iptables commands for HOTSPOT")
+        log.debug("iptmgr - Running nft commands for HOTSPOT")
 
-        run_cmds(end_cmds)
-        run_cmds(start_cmds)
+        stop_hs_rules()
+        start_hs_rules()
 
         if modemgr.get_mode() == modemgr.MULTI_MODE:
-            run_cmds(appliance_clear)
+            stop_router_rules()
 
-        log.debug("Done with iptables commands for HOTSPOT")
+        log.debug("iptmgr - Done with nft commands for HOTSPOT")
 
     elif (state, action) == ("CONNECTED", "start"):
-        log.debug("Running iptables commands for CONNECTED")
-        run_cmds(end_cmds)
+        log.debug("iptmgr - Running nft commands for CONNECTED")
+        stop_hs_rules()
 
         if modemgr.get_mode() == modemgr.MULTI_MODE:
-            run_cmds(appliance_clear)
-            run_cmds(appliance_cmds)
+            stop_router_rules()
+            start_router_rules()
+
             defaultdev: Optional[str] = routemgr.defroute_dev()
             apdev: str = modemgr.get_ap_device().Interface
             if defaultdev and defaultdev != apdev:
                 run_cmds(
                     [
-                        "iptables -w -t nat -I COMITUP-FWD -o "
-                        "{} -j MASQUERADE".format(defaultdev)
+                        f"nft 'insert rule ip nat COMITUP-FWD oifname \"{defaultdev}\" counter masquerade'"
                     ],
                 )
 
-        log.debug("Done with iptables commands for CONNECTED")
+        log.debug("iptmgr - Done with nft commands for CONNECTED")
 
 
 def init_iptmgr() -> None:
     pass
-
-
-def main():
-    print("applying rules")
-    run_cmds(start_cmds)
-
-    input("Press Enter to continue...")
-
-    run_cmds(end_cmds)
-
-
-if __name__ == "__main__":
-    main()
